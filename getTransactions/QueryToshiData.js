fe9374996156544718494d44bff9bca22ecb61f1
Max Schumacher <maximilianbschumacher@gmail.com>
2015-12-14 17:22:42 +0100

    Fixed query code, so large server responses are possible, too. Export filtered data for further processing

--- getTransactions/QueryToshiData.js
+++ getTransactions/QueryToshiData.js
@@ -3,54 +3,70 @@ var https = require('https'),
        fs = require('fs'),
         _ = require('lodash');
 
-// var dummyExchangeRate = 300.0;
-
 var blocks = [];
 var toshiBlockPath = "https://bitcoin.toshi.io/api/v0/blocks/";
-var toshiTxPath = "https://bitcoin.toshi.io/api/v0/transactions/";
-var transactions = [];
+
+// block 336861 first block of 2015.
+// block 381400 occured on October 31. 2015 (Bitcoin's Birthday)
+blockRange(336861, 337861);
 
 function blockRange(beginning, end) {
 	for (var height = beginning; height <= end; height++) {
 		// on average: 144 blocks per day.
-		if (height % 1 == 0) {
+		if (height % 72 == 0) {
 			blocks.push(new Promise(resolve => {
-				var url = toshiBlockPath + height.toString();
-				https.get(url, res => {
-				    res.setEncoding('utf8');
-				    res.on('data', d => {resolve(JSON.parse(d))});
-		    })
-            }));
-		}
+  				var url = toshiBlockPath + height.toString();
+  				https.get(url, res => {
+            res.setEncoding('utf8');
+            var tempBlockStorage = "";
+            res.on('data', d => {
+              tempBlockStorage += d;
+            })
+            res.on('end', () => {
+              resolve(JSON.parse(tempBlockStorage));
+            })
+          })
+      }));
+	  }
 	}
 }
 
-// block 336861 is the first block of 2015.
-// block 381400 occured on October 31. 2015 (Bitcoin's Birthday)
-blockRange(100001, 100001);
-
+var toshiTxPath = "https://bitcoin.toshi.io/api/v0/transactions/";
 var transactionHashes = [];
+var transactions = [];
 
 Promise.all(blocks).then(blocks => {
 	_.forEach(blocks, el => _.forEach(el.transaction_hashes, tx => {
         transactionHashes.push(tx);
-        _.forEach(transactionHashes, hash => {
-            var url = toshiTxPath + hash;
-            transactions.push(new Promise(resolve => https.get(url, res => {
-                res.setEncoding('utf8');
-                res.on('data', d => resolve(JSON.parse(d)));
-            })))
-        });
-
-        Promise.all(transactions).then(transactions => {
-            _.forEach(transactions, n => {
-                let wat = "volume: " + n.amount + ', ' + "fees: " + n.fees +"\n";
-
-                fs.appendFile("transactions", wat, 'utf8', (err) => {
-                    if (err) throw err;
-                } )
-
-            })
-	    })
     }))
+    for (let j = 1; j <= transactionHashes.length; j += 50) {
+      var url = toshiTxPath + transactionHashes[j];
+      transactions.push(new Promise(resolve => https.get(url, res => {
+          res.setEncoding('utf8');
+          var tempTxStorage = "";
+          res.on('data', d => {
+            tempTxStorage += d;
+          })
+          res.on('end', () => {
+            resolve(JSON.parse(tempTxStorage));
+          })
+      })))
+    }
+
+    let filteredTransactions = [];
+
+    Promise.all(transactions).then(transactions => {
+      _.forEach(transactions, tx => {
+          let pcFee = tx.fees / tx.amount;
+          filteredTransactions.push({
+            hash: tx.hash,
+            timestamp: tx.block_time,
+            volume: tx.amount,
+            transaction_fees: tx.fees,
+            percentage_fee: pcFee
+        });
+      });
+      //console.log(filteredTransactions)
+      module.exports = filteredTransactions;
+    })
 });
